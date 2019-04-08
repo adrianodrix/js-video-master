5fa3b9717e583c8257b6736527f812ec81697513
Adriano Santos <adrianodrix@gmail.com>
2019-04-08 17:15:27 -0300

    finish robot images

--- robots/image.js
+++ robots/image.js
@@ -1,5 +1,7 @@
 const { google } = require('googleapis')
 
+const imageDownloader = require('image-downloader') 
+
 const customSearch = google.customsearch('v1')
 const state = require('./state')
 
@@ -12,6 +14,12 @@ async function robot() {
       cx: googleSearchCredentials.searchEngineID,
       q: query,
       searchType: 'image',
+      // imgSize: 'large', 
+      // imgType: 'photo',
+      // imgColorType: 'color',      
+      // filter: '1',
+      // safe: 'active',
+      // rights: '(cc_publicdomain|cc_attribute|cc_sharealike)',     
       num: 2,
     })
 
@@ -28,9 +36,42 @@ async function robot() {
     await Promise.all(promises)
   }
 
+  async function downloadAndSave(url, fileName) {
+    return imageDownloader.image({
+      url, url,
+      dest: `./content/${fileName}`
+    })
+  }
+  
+  async function downloadAllImages(content) {
+    content.downloadedImages = []
+
+    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+      const images = content.sentences[sentenceIndex].images
+
+      for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+        const imageUrl = images[imageIndex]
+
+        try {
+          if (content.downloadedImages.includes(imageUrl)) {
+            throw new Error('Imagem já foi baixada.')
+          }
+
+          await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+          content.downloadedImages.push(imageUrl)
+          console.info(`> [${sentenceIndex}][${imageIndex}] Baixou imagem com sucesso: ${imageUrl}`)
+          break
+        } catch(error) {
+          console.error(`> [${sentenceIndex}][${imageIndex}] Erro ao baixar ${imageUrl}: ${error}`)
+        }
+      }
+    }
+  }
+
   const content = state.load()
 
   await fetchImagesOfAllSentences(content)
+  await downloadAllImages(content)
   
   state.save(content)
 }
